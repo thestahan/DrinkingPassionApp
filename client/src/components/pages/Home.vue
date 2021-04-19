08c68681512b294e62871a93213f9b715d949a8e
Kacper <thestahan@gmail.com>
2021-04-19 19:50:12 +0200

    Added home page; created basic routes

---
+++ client/src/components/pages/Home.vue
@@ -0,0 +1,192 @@
+<template>
+  <Popover class="relative bg-white overflow-hidden">
+    <div
+      class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+    >
+      <svg
+        class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+        fill="currentColor"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none"
+        aria-hidden="true"
+      >
+        <polygon points="50,0 100,0 50,100 0,100" />
+      </svg>
+
+      <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav
+          class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+          aria-label="Global"
+        >
+          <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div class="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span class="sr-only">Drinking Passion</span>
+                <img
+                  class="h-8 w-auto sm:h-10"
+                  src="https://www.svgrepo.com/show/119676/cocktail-glass.svg"
+                />
+              </a>
+              <div class="-mr-2 flex items-center md:hidden">
+                <PopoverButton
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Open main menu</span>
+                  <MenuIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+            <router-link
+              v-for="item in navigation"
+              :key="item.name"
+              :to="item.path"
+              class="font-medium text-gray-500 hover:text-gray-900"
+              >{{ item.displayName }}</router-link
+            >
+            <router-link
+              to="/login"
+              class="font-medium text-indigo-600 hover:text-indigo-500"
+              >Zaloguj się</router-link
+            >
+          </div>
+        </nav>
+      </div>
+
+      <transition
+        enter-active-class="duration-150 ease-out"
+        enter-from-class="opacity-0 scale-95"
+        enter-to-class="opacity-100 scale-100"
+        leave-active-class="duration-100 ease-in"
+        leave-from-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-95"
+      >
+        <PopoverPanel
+          focus
+          class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div
+            class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+          >
+            <div class="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img
+                  class="h-8 w-auto"
+                  src="https://www.svgrepo.com/show/119676/cocktail-glass.svg"
+                  alt=""
+                />
+              </div>
+              <div class="-mr-2">
+                <PopoverButton
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
+                  <span class="sr-only">Close main menu</span>
+                  <XIcon class="h-6 w-6" aria-hidden="true" />
+                </PopoverButton>
+              </div>
+            </div>
+            <div class="px-2 pt-2 pb-3 space-y-1">
+              <router-link
+                v-for="item in navigation"
+                :key="item.name"
+                :to="item.path"
+                class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                >{{ item.displayName }}</router-link
+              >
+            </div>
+            <router-link
+              to="/login"
+              class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+            >
+              Zaloguj się
+            </router-link>
+          </div>
+        </PopoverPanel>
+      </transition>
+
+      <main
+        class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+      >
+        <div class="sm:text-center lg:text-left">
+          <h1
+            class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+          >
+            <span class="block xl:inline">Najlepsze koktajle</span>
+            {{ " " }}
+            <span class="block text-indigo-600 xl:inline"
+              >w jednym miejscu</span
+            >
+          </h1>
+          <p
+            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+          >
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                Utwórz konto
+              </a>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+              >
+                Przejdź do koktajli
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <img
+        class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        src="@/assets/hero.jpg"
+        alt=""
+      />
+    </div>
+  </Popover>
+</template>
+
+<script>
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { MenuIcon, XIcon } from "@heroicons/vue/outline";
+
+export default {
+  components: {
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+    MenuIcon,
+    XIcon,
+  },
+  data() {
+    return {
+      navigation: [
+        {
+          displayName: "Strona główna",
+          name: "homePage",
+          path: "/",
+        },
+        {
+          displayName: "Koktajle",
+          name: "cocktails",
+          path: "/cocktails",
+        },
+      ],
+      open: false,
+    };
+  },
+  beforeCreate() {
+    this.$;
+  },
+};
+</script>
