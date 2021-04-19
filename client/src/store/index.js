1b1c80b53864409022f21212bcb0b0373da45a54
Kacper <thestahan@gmail.com>
2021-04-19 19:50:12 +0200

    #11 - Added home page; created basic routes

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,40 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    navigation: [
+      {
+        displayName: "Strona główna",
+        name: "homePage",
+        href: "/",
+        current: true,
+      },
+      {
+        displayName: "Koktajle",
+        name: "cocktails",
+        href: "/cocktails",
+        current: false,
+      },
+    ],
+    isAuthenticated: false,
+  },
+  getters: {
+    isAuthenticated: (state) => {
+      return state.isAuthenticated;
+    },
+  },
+  mutations: {
+    removeCurrentActivePage(state) {
+      const elIndex = state.navigation.findIndex((x) => x.current);
+      state.navigation[elIndex].current = false;
+    },
+    setActivePage(state, payload) {
+      const elIndex = state.navigation.findIndex(
+        (x) => x.name === payload.value
+      );
+      state.navigation[elIndex].current = true;
+    },
+  },
   actions: {},
   modules: {},
 });
