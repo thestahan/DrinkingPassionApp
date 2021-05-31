4804b5b57bb080a1e3d9bbbefcdd2fd080012d49
Kacper <thestahan@gmail.com>
2021-05-31 20:07:30 +0200

    #30 Added footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="max-w-7xl mx-auto">
+  <div class="flex flex-col max-w-7xl h-screen mx-auto justify-between">
     <navbar v-if="!isHomePage"></navbar>
-    <router-view />
+    <div class="mb-auto">
+      <router-view />
+    </div>
+    <app-footer class="h-10"></app-footer>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/layout/Navbar.vue";
+import AppFooter from "./components/layout/AppFooter.vue";
 
 export default {
   components: {
     Navbar,
+    AppFooter,
   },
   computed: {
     isAuthenticated() {
