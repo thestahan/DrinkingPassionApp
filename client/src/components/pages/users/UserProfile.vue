19a3cfa2d868da7f2418590604f9a51e3c12f7b9
Kacper <thestahan@gmail.com>
2021-06-24 21:54:40 +0200

    #26 Added bartender type information to profile

--- client/src/components/pages/users/UserProfile.vue
+++ client/src/components/pages/users/UserProfile.vue
@@ -1,7 +1,29 @@
 <template>
-  <h3>Profil</h3>
-  <p>{{ displayName }}</p>
-  <p>{{ email }}</p>
+  <header class="p-m-4">
+    <h3 class="p-text-center main-font secondary-heading-font">
+      Informacje o Tobie
+    </h3>
+  </header>
+
+  <section class="p-grid p-flex-column p-m-4" v-if="displayName">
+    <div class="p3-col p-mb-2">
+      <span class="p-text-bold primary-color">Nazwa wyświetlana:</span>
+      <p>{{ displayName }}</p>
+    </div>
+
+    <div class="p3-col p-mb-2">
+      <span class="p-text-bold primary-color">Adres email:</span>
+      <p>{{ email }}</p>
+    </div>
+
+    <div class="p3-col p-mb-2">
+      <span class="p-text-bold primary-color"
+        >Twoje doświadczenie w bramaństwie:</span
+      >
+      <p>{{ bartenderTypeName }}</p>
+    </div>
+  </section>
+
   <spinner v-if="isLoading"></spinner>
 </template>
 
@@ -15,7 +37,17 @@ export default {
       isLoading: false,
       displayName: null,
       email: null,
+      bartenderType: null,
     };
+  },
+  computed: {
+    bartenderTypeName() {
+      if (this.bartenderType === 2) {
+        return "Profesjonalista";
+      } else {
+        return "Zawodowiec";
+      }
+    },
   },
   created() {
     this.getUserProfile();
@@ -48,7 +80,15 @@ export default {
 
       this.displayName = responseData.displayName;
       this.email = responseData.email;
+      this.bartenderType = responseData.bartenderType;
     },
   },
 };
 </script>
+
+<style scoped>
+.secondary-heading-font {
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+</style>
