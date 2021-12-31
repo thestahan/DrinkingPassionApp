3ee7c1b6e52bf07c0ffd60c235fca90f462854f1
Kacper <thestahan@gmail.com>
2021-12-31 11:29:32 +0100

    #39 Add CocktailsLists migrations

--- Core/Entities/CocktailsList.cs
+++ Core/Entities/CocktailsList.cs
@@ -1,15 +1,15 @@
 ﻿using Core.Entities.Identity;
-using System;
 using System.Collections.Generic;
 
 namespace Core.Entities
 {
     public class CocktailsList : BaseEntity
     {
-        public Guid UniqueLink { get; set; }
+        public string UniqueLink { get; set; }
         public string Name { get; set; }
         public AppUser Author { get; set; }
         public string AuthorId { get; set; }
-        public IReadOnlyList<Cocktail> Cocktails { get; set; }
+        public ICollection<Cocktail> Cocktails { get; set; }
+        public int CocktailsCount { get; set; }
     }
 }
---
+++ Infrastructure/Data/Migrations/20211230120612_AddCocktailsLists.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddCocktailsLists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CocktailsLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UniqueLink = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    AuthorId = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CocktailsLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CocktailsLists_AspNetUsers_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CocktailCocktailsList",
+                columns: table => new
+                {
+                    CocktailsId = table.Column<int>(type: "integer", nullable: false),
+                    CocktailsListsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CocktailCocktailsList", x => new { x.CocktailsId, x.CocktailsListsId });
+                    table.ForeignKey(
+                        name: "FK_CocktailCocktailsList_Cocktails_CocktailsId",
+                        column: x => x.CocktailsId,
+                        principalTable: "Cocktails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CocktailCocktailsList_CocktailsLists_CocktailsListsId",
+                        column: x => x.CocktailsListsId,
+                        principalTable: "CocktailsLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailCocktailsList_CocktailsListsId",
+                table: "CocktailCocktailsList",
+                column: "CocktailsListsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailsLists_AuthorId",
+                table: "CocktailsLists",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CocktailCocktailsList");
+
+            migrationBuilder.DropTable(
+                name: "CocktailsLists");
+        }
+    }
+}
