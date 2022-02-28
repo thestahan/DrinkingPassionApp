4ae3937721e9d4d1c5341f8691272864347373c7
Kacper <thestahan@gmail.com>
2022-02-28 21:09:19 +0100

    #51 Add StringExtensions

---
+++ Core/Extensions/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveAccents(this string text)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            byte[] tempBytes;
+            tempBytes = Encoding.GetEncoding("ISO-8859-8").GetBytes(text);
+            string asciiStr = Encoding.UTF8.GetString(tempBytes);
+
+            return asciiStr;
+        }
+
+        public static string Slugify(this string phrase)
+        {
+            string output = phrase.RemoveAccents().ToLower();
+
+            // Remove all special characters from the string.  
+            output = Regex.Replace(output, @"[^A-Za-z0-9\s-]", "");
+
+            // Remove all additional spaces in favour of just one.  
+            output = Regex.Replace(output, @"\s+", " ").Trim();
+
+            // Replace all spaces with the hyphen.  
+            output = Regex.Replace(output, @"\s", "-");
+
+            return output;
+        }
+    }
+}
