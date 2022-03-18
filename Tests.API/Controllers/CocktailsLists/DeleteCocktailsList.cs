eefcb8a422bcf8117b804d84b9b681aeb2c6bfba
Kacper <thestahan@gmail.com>
2022-03-18 17:29:17 +0100

    #55 GetCocktailsListForGuest - add test for get/delete cocktailsList

---
+++ Tests.API/Controllers/CocktailsLists/DeleteCocktailsList.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Core.Entities.Identity;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Core.Specifications.CocktailsLists;
+
+namespace Tests.API.Controllers.CocktailsLists
+{
+    [TestFixture]
+    public class DeleteCocktailsList
+    {
+        private Mock<UserManager<AppUser>> _userManagerMock;
+        private Mock<IGenericRepository<CocktailsList>> _cocktailsListsRepoMock;
+        private Mock<IGenericRepository<Cocktail>> _cocktailsRepoMock;
+        private Mock<IMapper> _mapperMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var store = new Mock<IUserStore<AppUser>>();
+            _userManagerMock = new(store.Object, null, null, null, null, null, null, null, null);
+            _cocktailsListsRepoMock = new();
+            _cocktailsRepoMock = new();
+            _mapperMock = new();
+        }
+
+        [Test]
+        public async Task ReturnNotFoundCode_When_IdIsIncorrect()
+        {
+            // Arrange
+            var controller = new CocktailsListsController(_cocktailsListsRepoMock.Object,
+                                                          _userManagerMock.Object,
+                                                          _mapperMock.Object,
+                                                          _cocktailsRepoMock.Object);
+
+            //Act
+            var result = (await controller.DeleteCocktailsList(1)) as ObjectResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, result.StatusCode);
+        }
+
+        [Test]
+        public async Task ReturnNoContentCode_When_NoErrorsThrown()
+        {
+            // Arrange
+            _cocktailsListsRepoMock.Setup(c => c.GetEntityWithSpec(It.IsAny<CocktailsListForUserById>())).ReturnsAsync(new CocktailsList());
+
+            var controller = new CocktailsListsController(_cocktailsListsRepoMock.Object,
+                                                          _userManagerMock.Object,
+                                                          _mapperMock.Object,
+                                                          _cocktailsRepoMock.Object);
+
+            //Act
+            var result = (await controller.DeleteCocktailsList(1)) as StatusCodeResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual((int)HttpStatusCode.NoContent, result.StatusCode);
+        }
+    }
+}
---
+++ Tests.API/Controllers/CocktailsLists/GetCocktailsList.cs
@@ -0,0 +1,87 @@
+﻿using API.Controllers;
+using API.Dtos.CocktailsLists;
+using AutoMapper;
+using Core.Entities;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Core.Specifications.CocktailsLists;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Tests.API.Controllers.CocktailsLists
+{
+    [TestFixture]
+    public class GetCocktailsList
+    {
+        private Mock<UserManager<AppUser>> _userManagerMock;
+        private Mock<IGenericRepository<CocktailsList>> _cocktailsListsRepoMock;
+        private Mock<IGenericRepository<Cocktail>> _cocktailsRepoMock;
+        private Mock<IMapper> _mapperMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var store = new Mock<IUserStore<AppUser>>();
+            _userManagerMock = new(store.Object, null, null, null, null, null, null, null, null);
+            _cocktailsListsRepoMock = new();
+            _cocktailsRepoMock = new();
+            _mapperMock = new();
+        }
+
+        [Test]
+        public async Task ReturnsMappedList()
+        {
+            //Arrange
+            _cocktailsListsRepoMock.Setup(c => c.ListAsync(It.IsAny<CocktailsListsForUser>())).ReturnsAsync(GetTestCocktailsLists());
+            _mapperMock.Setup(m => m.Map<List<CocktailsListDto>>(It.IsAny<IEnumerable<CocktailsList>>())).Returns(GetMappedTestCocktailsLists());
+            _userManagerMock.Setup(u => u.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new AppUser { Id = "123" });
+
+            var controller = new CocktailsListsController(_cocktailsListsRepoMock.Object,
+                                                          _userManagerMock.Object,
+                                                          _mapperMock.Object,
+                                                          _cocktailsRepoMock.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext() { User = GetMockUser() }
+                }
+            };
+
+            //Act
+            var result = (await controller.GetCocktailsLists()).Result as ObjectResult;
+            var lists = result.Value as List<CocktailsListDto>;
+
+            //Assert
+            Assert.NotNull(lists);
+            Assert.IsInstanceOf(typeof(IEnumerable<CocktailsListDto>), lists);
+            Assert.AreEqual(lists.Count, 2);
+            Assert.AreEqual(lists[0].Id, 1);
+        }
+
+        private static List<CocktailsList> GetTestCocktailsLists() =>
+            new()
+            {
+                new CocktailsList { Id = 1 },
+                new CocktailsList { Id = 2 },
+            };
+
+        private static List<CocktailsListDto> GetMappedTestCocktailsLists() =>
+            new()
+            {
+                new CocktailsListDto { Id = 1 },
+                new CocktailsListDto { Id = 2 },
+            };
+
+        private static ClaimsPrincipal GetMockUser() =>
+            new(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Email, "mockEmail")
+            }));
+    }
+}
